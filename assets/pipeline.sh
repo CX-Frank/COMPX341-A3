471f36a6a3d52cbd75b9fadf2ff52bcc45eb8ba6
Feng, Chenxing <zxflx53@gmail.com>
2022-05-19 20:34:19 +1200

    COMPX341-22A- Add_commits_into_pipeline_add_the_student_name_and_studentID_to_every_ts_file

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -25,16 +25,16 @@ then
     echo 'Error: No commit message!'
     exit 0
 fi
-
+#Add the file
 git add -A
-
+#Add commit
 git commit -m "COMPX341-22A- ${1}"
 error_message 'git commit -m $'
-
+#Push file to Github
 git push origin main
 error_message 'git push origin master'
-
+#Retuan Success message
 echo 'Successfully deployed!'
-
+#Start running the software
 npm run start
 
